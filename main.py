30fe6bc2b715961bfd52709c07513b59bc6de809
Victor Lee <102740406+vvictorl@users.noreply.github.com>
2022-04-15 04:48:58 +0900

    Change branch from test/img-update to main

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     dh_img_tag = os.getenv("INPUT_TAGS")
     g = Github(gh_pat)
     repo = g.get_repo("Carina-labs/helm-charts")
-    contents = repo.get_contents(path="testnet/novachain-node/values.yaml", ref="test/img-update")
+    contents = repo.get_contents(path="testnet/novachain-node/values.yaml", ref="main")
     v_yml = yaml.load(str(base64.b64decode(contents.content), 'utf-8'), Loader=yaml.Loader)
     v_yml['image']['tag'] = dh_img_tag
     new_vyml = yaml.dump(v_yml)
     bot = InputGitAuthor('github-actions[bot]', 'github-actions[bot]@users.noreply.github.com')
     repo.update_file(path=contents.path, message="Update my img tag",
-                     content=new_vyml, sha=contents.sha, branch="test/img-update",
+                     content=new_vyml, sha=contents.sha, branch="main",
                      committer=bot)
