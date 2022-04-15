d507e1eb97eeb1c2852f9521f8f7b46da7905edf
victor <vvictorl@users.noreply.github.com>
2022-04-15 12:50:13 +0900

    Modify commit msg

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import base64
 
 if __name__ == "__main__":
     gh_pat = os.getenv("INPUT_A41_PAT")
-    dh_img_tag = os.getenv("INPUT_TAGS")
+    dh_img_tag = os.getenv("INPUT_TAGS").split(":")
+    dh_img_tag = dh_img_tag[1]
+
     g = Github(gh_pat)
     repo = g.get_repo("Carina-labs/helm-charts")
     contents = repo.get_contents(path="testnet/novachain-node/values.yaml", ref="main")
@@ -14,6 +16,6 @@ if __name__ == "__main__":
     v_yml['image']['tag'] = dh_img_tag
     new_vyml = yaml.dump(v_yml)
     bot = InputGitAuthor('github-actions[bot]', 'github-actions[bot]@users.noreply.github.com')
-    repo.update_file(path=contents.path, message="Update my img tag",
+    repo.update_file(path=contents.path, message="ci: update novachain docker image",
                      content=new_vyml, sha=contents.sha, branch="main",
                      committer=bot)
